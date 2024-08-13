01f3274819003861c7518fb9b30f29bc43f8280c
unknown <Qusai Halaseh@Polaris007.polaris.local>
2024-08-13 15:14:31 +0300

    scound

--- mydb.py
+++ mydb.py
@@ -2,21 +2,16 @@
 # https://dev.mysql.com/downloads/installer/
 # pip install mysql
 # pip install mysql-connector
-# pip install mysql-connector-python 
+# pip install mysql-connector-python
 
 import mysql.connector
 
-dataBase = mysql.connector.connect(
-	host = 'localhost',
-	user = 'root',
-	passwd = 'password123'
-
-	)
+dataBase = mysql.connector.connect(host="localhost", user="root", passwd="password123")
 
 # prepare a cursor object
 cursorObject = dataBase.cursor()
 
 # Create a database
-cursorObject.execute("CREATE DATABASE elderco")
+cursorObject.execute("CREATE DATABASE Qusai")
 
 print("All Done!")
